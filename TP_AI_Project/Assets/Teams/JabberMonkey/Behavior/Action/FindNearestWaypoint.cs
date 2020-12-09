6f075d3cd0ded896b009601c0a657fc179ac61af
Matt <matthieu.manuellan@gmail.com>
2020-12-09 15:26:03 +0100

    FindNearestWaypoint

---
+++ TP_AI_Project/Assets/Teams/JabberMonkey/Behavior/Action/FindNearestWaypoint.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using DoNotModify;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using System.Collections.Generic;
+
+namespace JabberMonkey
+{
+    public class FindNearestWaypoint : Action
+    {
+		private List<Vector3> waypointList;
+		private BlackBordScipt blackboard;
+		public override void OnStart()
+		{
+			blackboard = GetComponent<BlackBordScipt>();
+			foreach(WayPoint p in GameManager.Instance.GetGameData().WayPoints)
+            {
+				if(p.Owner != blackboard.shipIndex)
+					waypointList.Add(p.Position);
+            }
+			if(waypointList.Count > 1)
+				waypointList.Sort((x, y) => Vector3.Distance(x, blackboard.myShip.transform.position).CompareTo(Vector3.Distance(y, blackboard.myShip.transform.position)));
+
+		}
+
+		public override TaskStatus OnUpdate()
+		{
+				return TaskStatus.Success;
+		}
+	}
+}
