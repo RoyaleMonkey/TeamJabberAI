8d26960084dd829a8aa6cc054b21b232251ee8ce
Cedric CHAUVIN <cedric.chauvin@edu.devinci.fr>
2020-12-10 16:28:53 +0100

    add shoot timer

--- TP_AI_Project/Assets/Teams/JabberMonkey/Behavior/Action/ShootForward.cs
+++ TP_AI_Project/Assets/Teams/JabberMonkey/Behavior/Action/ShootForward.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
+using System.Collections;
 
 namespace JabberMonkey
 {
 	[TaskCategory("JabberAI")]
 	public class ShootForward : Action
 	{
+		public float cooldown = 0.05f;
 
+		private bool canShoot = true;
 		BlackBordScipt blackBord = null;
 
 		public override void OnAwake()
@@ -17,12 +20,23 @@ namespace JabberMonkey
 
 		public override void OnStart()
 		{
-			blackBord.shouldShoot = true;
 		}
 
 		public override TaskStatus OnUpdate()
 		{
+			if (!canShoot)
+				return TaskStatus.Failure;
+
+			blackBord.shouldShoot = true;
+			StartCoroutine(ShootRoutine());
 			return TaskStatus.Success;
+		}
+
+		private IEnumerator ShootRoutine()
+		{
+			canShoot = false;
+			yield return new WaitForSeconds(cooldown);
+			canShoot = true;
 		}
 	}
 }
