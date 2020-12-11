761e37f515f80b7322558d6abc482f038a8fe177
Cedric CHAUVIN <cedric.chauvin@edu.devinci.fr>
2020-12-11 08:50:41 +0100

    target Enemy check waypoint on path

--- TP_AI_Project/Assets/Teams/JabberMonkey/Behavior/Action/TargetEnemy.cs
+++ TP_AI_Project/Assets/Teams/JabberMonkey/Behavior/Action/TargetEnemy.cs
@@ -8,6 +8,9 @@ namespace JabberMonkey
 	public class TargetEnemy : Action
 	{
 
+		public float checkDistance;
+		public float forwardMultiplier;
+
 		private Transform myTransform;
 		private BlackBordScipt blackBordScipt;
 
@@ -18,6 +21,20 @@ namespace JabberMonkey
 
 		public override TaskStatus OnUpdate()
 		{
+            foreach (WayPoint item in GameManager.Instance.GetGameData().WayPoints)
+            {
+				if(item.Owner != blackBordScipt.shipIndex)
+                {
+					Vector2 forward = blackBordScipt.myShip.transform.right;
+					float Distance = ((blackBordScipt.myShip.Position + forward * forwardMultiplier) - item.Position).magnitude;
+					if (Distance < checkDistance)
+					{
+						blackBordScipt.targetPosition = item.Position;
+						return TaskStatus.Success;
+					}
+
+				}
+            }
 			blackBordScipt.targetPosition = blackBordScipt.enemyBackPosition;
 			return TaskStatus.Success;
 		}
